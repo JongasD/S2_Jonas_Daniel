4f0dfb2f4650f56744a142a220356157dd4191e7
JongasD <lopesjonasdaniel@gmail.com>
2025-04-08 09:47:45 -0300

    Atualização quitanda

--- Quitanda/quitanda.py
+++ Quitanda/quitanda.py
@@ -7,13 +7,13 @@ print("""
         - Cebola
         - Laranja
         - Melancia
-        - 
+        - Chinelo
         - Kiwi
       Para acessar um produto, digite a letra inicial do produto.
 """)
 def acessar_produto(): #Função para acessar o produto 
-    print("Alface 1,00 cada unidade")
     produto = input("Digite a letra inicial do produto: ") #Pode ser maiuscula ou minuscula
+    print("Alface 1,00 cada unidade")
     if produto == "T" or produto == "t":
         print("Tomates: 1,50 cada unidade")
     elif produto == "A" or produto == "a":
@@ -21,17 +21,17 @@ def acessar_produto(): #Função para acessar o produto
     elif produto == "B" or produto == "b":
         print("Batata 1,20 cada unidade")
     elif produto == "C" or produto == "c":
-        print("Cebola  cada unidade")
+        print("Cebola 100000000000000000000000000000000000 cada unidade")
     elif produto == "L" or produto == "l":
         print("Laranja 2,00 cada unidade")
     elif produto == "M" or produto == "m":
         print("Melancia 25,00 cada unidade")
     elif produto == "F" or produto == "f":
         print("Feijao 4,00 cada unidade")
-    elif produto == "K" or produto == "":
+    elif produto == "K" or produto == "w":
         print("Kiwi 5,00 cada unidade")
     else:
         print("Produto não encontrado")
 
 acessar_produto()
-print("Muito obrigado pela visita em nossa ")
+print("Muito obrigado pela visita em nossa padaria!")
